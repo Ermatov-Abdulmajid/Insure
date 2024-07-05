0679afacd0ec8032262db100657507312c10ce1f
Abdulmajid <ermatovabdumajid23@gmail.com>
2024-07-05 09:35:05 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Header from "./components/Header.jsx";
+import Intro from "./components/Intro.jsx";
+import Features from "./components/features.jsx";
+import Cta from "./components/cta.jsx";
+import Footer from "./components/footer.jsx";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Intro />
+      <Features />
+      <Cta />
+      <Footer />
+    </>
+  );
+};
+
+export default App;
